012bbdd897d635471d74e0bf5a12e2e1f7240899
bilal2134 <i210575@nu.edu.pk>
2024-07-07 02:52:32 +0500

    first commit

---
+++ data/index.ts
@@ -0,0 +1,249 @@
+import { CiInstagram } from "react-icons/ci";
+
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title:
+      "Currently working on revolutionary HR & payroll software using .Net core and ANGULAR.",
+    description: "The Inside Scoop: Enwage",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.png",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Cafe management System C#",
+    des: "using MsSQL and c# windows forms created a 3 different role based authentications.",
+    img: "/p1.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "github.com",
+  },
+  {
+    id: 2,
+    title: "Job seeking MERN stack website",
+    des: "A complete job posting and product showcase website with various different modules.",
+    img: "/p2.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "github.com",
+  },
+  {
+    id: 3,
+    title: "Courrier management system JavaFX",
+    des: "Using JAVA FX, MySQL and following MVC architecture made a simple CMS which fullfils all basic stuff .",
+    img: "/p3.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "github.com",
+  },
+  {
+    id: 4,
+    title: "AI Timetable Generator",
+    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    img: "/p4.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "github.com",
+  },
+  {
+    id: 5,
+    title: "Data scraping and web crawling scripts",
+    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    img: "/p5.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "github.com",
+  },
+  {
+    id: 6,
+    title: "Parallax Frontend webstie",
+    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    img: "/p6.gif",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "github.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Bilal was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Bilal's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Bilal is the ideal partner.",
+    name: "Arham Akmal",
+    title: "CEO and Founder of Wanderlust",
+    image: "/Arham.jpg", // Add the image path here
+  },
+  {
+    quote:
+      "Working with Bilal Shakeel on our e-commerce platform was an absolute pleasure. His expertise in both front-end and back-end technologies ensured a seamless user experience and robust server-side performance. Bilal's ability to quickly understand our requirements and implement effective solutions was instrumental in the successful launch of our project. His innovative approach and commitment to excellence truly set him apart.",
+    name: "Zinoor Fatima",
+    title: "CTO SRM Technologies",
+    image: "/zinoor.jpg", // Add the image path here
+  },
+  {
+    quote:
+      "As a full stack engineer, Bilal Shakeel played a critical role in developing our cloud-based healthcare management system. His proficiency in multiple technology stacks, combined with his meticulous attention to detail, ensured that our platform was both scalable and secure. Bilal's dedication to quality and his ability to work under tight deadlines were key factors in the project's success. He's an exceptional developer and a true asset to any team.",
+    name: "Eeman Ijaz",
+    title: "US client",
+    image: "/eeman.jpg", // Add the image path here
+  },
+  {
+    quote:
+      "Bilal Shakeel's contribution to our AI-driven analytics dashboard was invaluable. His deep understanding of data science and web development allowed us to create an intuitive and powerful tool that significantly improved our business intelligence capabilities. Bilal's collaborative spirit and proactive problem-solving skills made him an essential part of our team. I highly recommend him for any complex web development projects.",
+    name: "Hassan Abbas",
+    title: "Director of Yapster Technologies",
+    image: "/hassan.jpg", // Add the image path here
+  },
+  {
+    quote:
+      "Bilal Shakeel's work on our social media integration project exceeded all expectations. His expertise in various web technologies and frameworks allowed us to seamlessly integrate multiple social platforms into our application, enhancing user engagement and satisfaction. Bilal's creativity, combined with his technical acumen, made the entire development process smooth and efficient. I look forward to collaborating with him on future projects.",
+    name: "Hayat Sikandar",
+    title: "Ex minister of Sindh Pakistan",
+    image: "/hayat.jpg", // Add the image path here
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Full Stack Web Developer - Oak streets",
+    desc: "With my expertise in various tech stacks, namely MEAN and MERN, I have successfully delivered robust and scalable web applications. My proficiency in both front-end and back-end development ensures seamless and efficient project execution. .",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "AI and Data scraping expert - Data Insights",
+    desc: "With my expertise in AI and data scraping, I have developed intelligent systems that efficiently extract and analyze web data. This combination ensures accurate data collection and insightful analytics, driving informed decision-making for various projects.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance mobile App Dev Project",
+    desc: "With my expertise in Flutter, I have developed seamless, cross-platform mobile applications that deliver a native performance and user experience. My proficiency in Flutter ensures rapid development and deployment, enabling efficient and visually appealing app solutions for various project needs.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Strong logic building and problem solving skills",
+    desc: "With my expertise in .NET architecture, Java, and C++, I have designed and implemented robust software solutions across various platforms. My proficiency in these technologies ensures efficient, scalable, and high-performance applications tailored to meet diverse project requirements.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    link: "https://twitter.com/thebilalshakeel",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/bilal-shakeel-ab2145279/",
+  },
+  {
+    id: 4,
+    img: "https://th.bing.com/th/id/R.1d19412811af9d2fa56516f0a2a3d0d3?rik=QrfTUipO6trsRA&pid=ImgRaw&r=0",
+    link: "https://www.instagram.com/bilal_shakeel8/",
+  },
+];
